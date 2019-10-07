9c413d04fe5741100052f76354e138f8c7d028e7
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2019-10-07 15:42:01 -0300

    initial page css ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import api from './services/api';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
+
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+      <div className="content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para seu
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email" >E-MAIL</label>
+          <input
+          id="email"
+          type="email"
+          placeholder="Seu melhor e-mail"
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
