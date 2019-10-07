ad0e88a9c523d3af310c0aecf7c04f01636d4b59
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2019-10-07 16:05:52 -0300

    Puting routes in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,19 @@
 import React from 'react';
-import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
 
   
-  function handleSubmit(event) {
-    event.preventDefault();
-    
-    
-  }
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
+
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para seu
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email" >E-MAIL</label>
-          <input
-          id="email"
-          type="email"
-          placeholder="Seu melhor e-mail"
-          />
-
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
